15e88fb4c4728cc600f0e84b0782d70308825f92
Laiba <laiba.aliraza2203@gmail.com>
2025-07-08 18:53:36 +0500

    Update second_try.sh

--- second_try.sh
+++ second_try.sh
@@ -7,8 +7,8 @@ then
      exit 1
 fi
 
-dir="$1"#first argument 
-keyword="$2"#second argument
+dir="$1"   #first argument 
+keyword="$2"    #second argument
 
 log_file="log/log_actions.log"
 #log_file stores,search and backup all activities
@@ -16,16 +16,18 @@ log_file="log/log_actions.log"
 time=$(date +"%Y-%m-%d %H:%M:%S")
 #format to get current date or time
 
-files=()#empty array
+files=()     #empty array
 
-echo "searching for Keyword \ "$keyword\" in directory\ "$dir\""
 #print that it search for the given word in given directory
+echo "searching for Keyword \"$keyword\" in directory\ "$dir\""
 
-word-match=$(grep -rnw "$dir" -e "$keyword" 2>/dev/null)
+
 #search the given word in given directory and hide if any error happens
+word_match=$(grep -rnw "$dir" -e "$keyword" 2>/dev/null)
+
 
 #if no match is found then exit and log the result
-if [ -z "$word-match" ]
+if [ -z "$word_match" ]
 then 
      echo "no words match(empty)"
      echo "$time | search: 0 files matched keyword \"$keyword\"" >> "$log_file"
@@ -37,7 +39,7 @@ while IFS=read -r line;
 do
    file=$(echo "$line" | cut -d: -f1)
    files+=("$file")
-done <<< "$word-match"
+done <<< "$word_match"
 
 #turn space separated list one per line and sort and remove duplicate
 unique_files=($(echo "${files[@]}" | tr ' ' '\n' | sort -u))
@@ -46,7 +48,7 @@ unique_files=($(echo "${files[@]}" | tr ' ' '\n' | sort -u))
 echo "$time | Search: ${#unique_files[@]} files matches keyword \"$keyword\"" >> "$log_file"
 
 #print matched word
-echo"$word-match"
+echo"$word_match"
 
 #make unique name
 archive_name="archive_$(date +"%Y%m%d_%H%M%S").tar.gz"
